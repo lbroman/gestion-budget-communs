a27ff308a0b68a003c3536bff2d4a50b410b9ed0
ZWINGMANN Vincent <vincent.zwingmann@soprasteria.com>
2018-09-11 13:33:29 +0200

    update des URL

--- src/main/java/com/terrier/finances/gestion/communs/utils/data/BudgetApiUrlEnum.java
+++ src/main/java/com/terrier/finances/gestion/communs/utils/data/BudgetApiUrlEnum.java
@@ -6,42 +6,43 @@ package com.terrier.finances.gestion.communs.utils.data;
  *
  */
 public class BudgetApiUrlEnum {
-	
-	
+
+
 	private BudgetApiUrlEnum(){
 		// Constructeur privé pour une classe enum
 	}
-	public static final String ROOT_BASE = "/rest";
-
 	/**
 	 * Statut
 	 */
-	public static final String STATUT_BASE = "/statut";
-	
+	public static final String ADMIN_BASE = "/admin";	
+	public static final String ADMIN_STATUT = "/v1/statut";
+	public static final String ADMIN_STATUT_FULL = ADMIN_BASE + ADMIN_STATUT;
+	public static final String ADMIN_PASSWORD = "/v1/password";
+	public static final String ADMIN_PASSWORD_FULL = ADMIN_BASE + ADMIN_PASSWORD;
 	/**
 	 * Authentification / Utilisateurs
 	 */
 	public static final String USERS_BASE = "/utilisateurs";
-	
+
 	public static final String USERS_AUTHENTICATE = "/v1/authenticate";
 	public static final String USERS_AUTHENTICATE_FULL = USERS_BASE + USERS_AUTHENTICATE;
-	
-	
+
+
 	public static final String USERS_DISCONNECT = "/v1/disconnect";
 	public static final String USERS_DISCONNECT_FULL = USERS_BASE + USERS_DISCONNECT;
-	
+
 	public static final String USERS_ACCESS_DATE = "/v1/lastaccessdate";
 	public static final String USERS_ACCESS_DATE_FULL = USERS_BASE + USERS_ACCESS_DATE;
 	public static final String USERS_PREFS = "/v1/preferences";
 	public static final String USERS_PREFS_FULL = USERS_BASE + USERS_PREFS;
-	
+
 	/**
 	 * Comptes
 	 */
 	public static final String COMPTES_BASE = "/comptes";
 	public static final String COMPTES_LIST = "/v1/list";
 	public static final String COMPTES_LIST_FULL = COMPTES_BASE + COMPTES_LIST;
-	
+
 	public static final String COMPTES_ID = "/v1";
 	public static final String COMPTES_ID_FULL = COMPTES_BASE + COMPTES_ID;
 }
