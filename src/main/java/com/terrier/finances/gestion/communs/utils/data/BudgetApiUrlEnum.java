5d695b5bad741aaf1bd6e28f8024ceca41fb5609
ZWINGMANN Vincent <vincent.zwingmann@soprasteria.com>
2018-09-17 22:13:39 +0200

    suppression des idUtilisateurs dans les URL. Passées via JWT

--- src/main/java/com/terrier/finances/gestion/communs/utils/data/BudgetApiUrlEnum.java
+++ src/main/java/com/terrier/finances/gestion/communs/utils/data/BudgetApiUrlEnum.java
@@ -17,7 +17,7 @@ public class BudgetApiUrlEnum {
 	public static final String ADMIN_BASE = "/admin";	
 	public static final String ADMIN_STATUT = "/v1/statut";
 	public static final String ADMIN_STATUT_FULL = ADMIN_BASE + ADMIN_STATUT;
-	public static final String ADMIN_ACCESS = "/v1/password";
+	public static final String ADMIN_ACCESS = "/v1/password/{oldpassword}/{newpassword}";
 	public static final String ADMIN_ACCESS_FULL = ADMIN_BASE + ADMIN_ACCESS;
 	/**
 	 * Authentification / Utilisateurs
@@ -28,28 +28,28 @@ public class BudgetApiUrlEnum {
 	public static final String USERS_AUTHENTICATE_FULL = USERS_BASE + USERS_AUTHENTICATE;
 
 
-	public static final String USERS_DISCONNECT = "/{idUtilisateur}/disconnect";
+	public static final String USERS_DISCONNECT = "/disconnect";
 	public static final String USERS_DISCONNECT_FULL = USERS_BASE + USERS_DISCONNECT;
 
-	public static final String USERS_ACCESS_DATE = "/{idUtilisateur}/lastaccessdate";
+	public static final String USERS_ACCESS_DATE = "/lastaccessdate";
 	public static final String USERS_ACCESS_DATE_FULL = USERS_BASE + USERS_ACCESS_DATE;
-	public static final String USERS_PREFS = "/{idUtilisateur}/preferences";
+	public static final String USERS_PREFS = "/preferences";
 	public static final String USERS_PREFS_FULL = USERS_BASE + USERS_PREFS;
 
 	/**
 	 * Comptes
 	 */
 	public static final String COMPTES_BASE = "/comptes/v1";
-	public static final String COMPTES_LIST = "/{idUtilisateur}";
+	public static final String COMPTES_LIST = "/";
 	public static final String COMPTES_LIST_FULL = COMPTES_BASE + COMPTES_LIST;
 
-	public static final String COMPTES_ID = "/{idCompte}/{idUtilisateur}";
+	public static final String COMPTES_ID = "/{idCompte}";
 	public static final String COMPTES_ID_FULL = COMPTES_BASE + COMPTES_ID;
 
 	public static final String COMPTES_INTERVALLES = "/{idCompte}/intervalles";
 	public static final String COMPTES_INTERVALLES_FULL = COMPTES_BASE + COMPTES_INTERVALLES;
 	
-	public static final String COMPTES_OPERATIONS_LIBELLES = "/{idCompte}/{idUtilisateur}/operations/libelles";
+	public static final String COMPTES_OPERATIONS_LIBELLES = "/{idCompte}/operations/libelles";
 	public static final String COMPTES_OPERATIONS_LIBELLES_FULL = COMPTES_BASE + COMPTES_OPERATIONS_LIBELLES;
 	
 	
@@ -65,17 +65,17 @@ public class BudgetApiUrlEnum {
 	 * Budget
 	 */
 	public static final String BUDGET_BASE = "/budgets/v1";	
-	public static final String BUDGET_ID = "/{idBudget}/{idUtilisateur}";
+	public static final String BUDGET_ID = "/{idBudget}";
 	public static final String BUDGET_ID_FULL = BUDGET_BASE + BUDGET_ID;
 
 	/*
-	 * Avec en paramètre ?idCompte={idCompte}&mois={mois}&annee={annee}&idUtilisateur={idUtilisateur}
+	 * Avec en paramètre ?idCompte={idCompte}&mois={mois}&annee={annee}
 	 */
 	public static final String BUDGET_QUERY = "/query"; 
 	public static final String BUDGET_QUERY_FULL = BUDGET_BASE + BUDGET_QUERY;
 	/*
 	 * Avec en paramètres :  ?actif=true  ou ?uptodateto=long
 	 */
-	public static final String BUDGET_ETAT = "/{idBudget}/{idUtilisateur}/etat"; 
+	public static final String BUDGET_ETAT = "/{idBudget}/etat"; 
 	public static final String BUDGET_ETAT_FULL = BUDGET_BASE + BUDGET_ETAT;
 }
